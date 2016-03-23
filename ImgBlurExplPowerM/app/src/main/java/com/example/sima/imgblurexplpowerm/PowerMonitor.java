b6f96afa91a66ca90b71bcb45d6bccce738309f8
Daehyeok Mun <daehyeok@gmail.com>
2016-03-23 14:23:25 -0600

    Chaged to get option from server

--- ImgBlurExplPowerM/app/src/main/java/com/example/sima/imgblurexplpowerm/PowerMonitor.java
+++ ImgBlurExplPowerM/app/src/main/java/com/example/sima/imgblurexplpowerm/PowerMonitor.java
@@ -1,9 +1,14 @@
 package com.example.sima.imgblurexplpowerm;
 
+import android.os.SystemClock;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,29 +17,58 @@ import java.net.URL;
  * Created by daehyeok on 2016. 3. 21..
  */
 public class PowerMonitor {
-    static boolean usePowerMonitor = false;
+    static boolean usePowerMonitor = true;
     static String baseUrl = "http://10.8.0.1:8000";
 
-    PowerMonitor() {
+    PowerMonitor() {};
+
+
+    private HttpURLConnection sendRequest(final String apiUrl){
+
+        class RequestThread extends Thread{
+            public HttpURLConnection con;
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(apiUrl);
+                    Log.d("Power Monitoring", "Sending request to URL : " + url);
+                    con = (HttpURLConnection) url.openConnection();
+                    Log.d("Power Monitoring", "Response Code : " + con.getResponseCode());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        try {
+            while(true) {
+                RequestThread thread = new RequestThread();
+                thread.start();
+                thread.join();
+
+                if (thread.con.getResponseCode() == 200){
+                    return thread.con;
+                }
+                Log.d("Power Monitoring", "Power Monitor have a Problem Waiting 30 sec");
+                SystemClock.sleep(10000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
-
-    ;
-
-    public static void setBaseUrl(String url) {
+    public void setBaseUrl(String url) {
         baseUrl = url;
     }
 
-    public static boolean startMonitoring() {
+    public boolean  startMonitoring() {
         Log.i("Power Monitoring", "Power Monitor start");
         if (!usePowerMonitor) return true;
 
-        URL url;
         try {
-            url = new URL(baseUrl + "/start");
-
-            Log.d("Power Monitoring", "Sending Start request to URL : " + url);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            Log.d("Power Monitoring", "Response Code : " + con.getResponseCode());
+            sendRequest(baseUrl + "/start");
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -43,18 +77,12 @@ public class PowerMonitor {
         return true;
     }
 
-    public static boolean saveMonitoring(String filename) {
-        Log.i("Power Monitoring", "Power Monitor stop : request " + filename);
+    public boolean saveMonitoring(String filename) {
+        Log.i("Power Monitoring", "Power Monitor stop : " + filename);
         if (!usePowerMonitor) return true;
 
-        URL url;
         try {
-            url = new URL(baseUrl + "/save?file=" + filename);
-
-            Log.d("Power Monitoring", "Sending Save request to URL : " + url);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-            Log.d("Power Monitoring", "Response Code : " + con.getResponseCode());
+            sendRequest(baseUrl + "/save/" + filename);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -63,5 +91,40 @@ public class PowerMonitor {
         return true;
     }
 
+    public String getTarget() {
+        Log.i("Power Monitoring", "Trying to get target");
 
+        String TargetString = "";
+
+        if (!usePowerMonitor) return TargetString;
+
+
+
+
+        try {
+            HttpURLConnection con = sendRequest(baseUrl + "/target");
+
+            InputStream in = new BufferedInputStream(con.getInputStream());
+            TargetString = readStream(in);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return TargetString;
+    }
+
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while (i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
 }
