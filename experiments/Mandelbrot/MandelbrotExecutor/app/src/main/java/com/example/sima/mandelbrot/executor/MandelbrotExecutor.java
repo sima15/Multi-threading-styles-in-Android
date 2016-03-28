36b672a459519f0162679cc24f1c6cbd5558c3ec
sima15 <s.mehry.89@gmail.com>
2016-03-28 11:32:31 -0600

    Refactored Mandelbrot handler runnable, message and Executor

--- experiments/Mandelbrot/MandelbrotExecutor/app/src/main/java/com/example/sima/mandelbrot/executor/MandelbrotExecutor.java
+++ experiments/Mandelbrot/MandelbrotExecutor/app/src/main/java/com/example/sima/mandelbrot/executor/MandelbrotExecutor.java
@@ -1,9 +1,7 @@
 package com.example.sima.mandelbrot.executor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
 import java.util.concurrent.ExecutorService;
@@ -11,11 +9,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ *
+ * @author Sima Mehri
+ */
 public class MandelbrotExecutor extends AppCompatActivity {
     long startTime;
-    long endTime;
     long totalTime;
-    int N;
+    int N = 500;
     EditText inputText;
 
     byte[][] out;
@@ -29,67 +30,53 @@ public class MandelbrotExecutor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mandelbrot_executor);
 
-        Button submitButton = (Button) findViewById(R.id.submitButton);
-        inputText = (EditText) findViewById(R.id.inputText);
+        startTime = System.currentTimeMillis();
+        System.out.println("start time is: " + String.valueOf(startTime));
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startTime = System.currentTimeMillis();
-                System.out.println("start time is: " + String.valueOf(startTime));
+        inputText = (EditText)findViewById(R.id.inputText);
 
-                String inputStr = inputText.getText().toString();
-                N = Integer.parseInt(inputStr);
+        //To increase the running time of the application
+        for (int i=0; i<800; i++) doJob();
 
-                Crb = new double[N + 7];
-                Cib = new double[N + 7];
-                double invN = 2.0 / N;
-                for (int i = 0; i < N; i++) {
-                    Cib[i] = i * invN - 1.0;
-                    Crb[i] = i * invN - 1.5;
-                }
-                yCt = new AtomicInteger();
-                out = new byte[N][(N + 7) / 8];
-
-//                int loopCount =  (Runtime.getRuntime().availableProcessors())*16;
-                int poolLength = 4;
-                ExecutorService executor = null;
-
-                //To increase the running time of the application
-                for (int j=1; j<=800; j++) {
-                    executor = Executors.newFixedThreadPool(poolLength);
-                    for (int i = 0; i < poolLength; i++) {
-
-                        executor.submit(new Runnable() {
-                            @Override
-                            public void run() {
-                                int y ;
-//                                long innerStartTime = System.currentTimeMillis();
-                                while ((y = yCt.getAndIncrement()) < out.length) {
-                                    putLine(y, out[y]);
-                                }
-                            }
-                        });
-                    }
-                    executor.shutdown();
-                    try {
-                        executor.awaitTermination(500, TimeUnit.SECONDS);
-
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if(executor.isTerminated()) {
-                    endTime = System.currentTimeMillis();
-                    totalTime = endTime - startTime;
-                    inputText.setText(String.valueOf(totalTime));
-                    System.out.println("end time is: " + endTime);
-                    System.out.println("Total time is: " + totalTime);
-                }
-            }
-        });
+        totalTime = System.currentTimeMillis() - startTime;
+        inputText.setText(String.valueOf(totalTime));
+        System.out.println("Total time is: " + totalTime);
     }
 
+    protected  void doJob(){
+        Crb = new double[N + 7];
+        Cib = new double[N + 7];
+        double invN = 2.0 / N;
+        for (int i = 0; i < N; i++) {
+            Cib[i] = i * invN - 1.0;
+            Crb[i] = i * invN - 1.5;
+        }
+        yCt = new AtomicInteger();
+        out = new byte[N][(N + 7) / 8];
+
+        int poolLength = 4;
+        ExecutorService executor;
+
+            executor = Executors.newFixedThreadPool(poolLength);
+            for (int i = 0; i < poolLength; i++) {
+
+                executor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        int y ;
+                        while ((y = yCt.getAndIncrement()) < out.length) {
+                            putLine(y, out[y]);
+                        }
+                    }
+                });
+            }
+            executor.shutdown();
+            try {
+                executor.awaitTermination(500, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    }
 
     static int getByte(int x, int y) {
         int res = 0;
