1d59ff011567e91a790a3420a89cc58a69329c1c
sima15 <s.mehry.89@gmail.com>
2016-03-28 10:09:13 -0600

    Refactored mandelbrot explicit and forkjoin

--- experiments/Mandelbrot/Mandelbrot/app/src/main/java/com/example/assl_blue/dacapo_benchmark/Mandelbrot1.java
+++ experiments/Mandelbrot/Mandelbrot/app/src/main/java/com/example/assl_blue/dacapo_benchmark/Mandelbrot1.java
@@ -2,15 +2,13 @@ package com.example.assl_blue.dacapo_benchmark;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Mandelbrot1 extends AppCompatActivity {
 
-    int N ;
+    int N = 500;
     long startTime;
     long endTime = startTime;
     long totalTime;
@@ -31,73 +29,67 @@ public class Mandelbrot1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mandelbrot1);
 
-        Button submitButton = (Button) findViewById(R.id.submitButton);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 startTime = System.currentTimeMillis();
                 System.out.println("Start time is: " + startTime);
                 inputText = (EditText) findViewById(R.id.inputText);
-                N = Integer.parseInt(String.valueOf(inputText.getText()));
 
-                Crb = new double[N + 7];
-                Cib = new double[N + 7];
-                double invN = 2.0 / N;
-                for (int i = 0; i < N; i++) {
-                    Cib[i] = i * invN - 1.0;
-                    Crb[i] = i * invN - 1.5;
-                }
-                yCt = new AtomicInteger();
-                out = new byte[N][(N + 7) / 8];
+                for(int i=0; i<800; i++) doJob();
 
-                poolLength = 1;
-                threads = new boolean[poolLength];
-                for (int loopCount = 0; loopCount < 800; loopCount++) {
-                    pool = new Thread[poolLength];
-                    for (int i = 0; i < pool.length; i++) {
-                        pool[i] = new Thread() {
-                            int temp =0;
-                            public void run() {
-                                    int y;
-                                    while ((y = yCt.getAndIncrement()) < out.length) {
-                                        putLine(y, out[y]);
-                                    }
-                                     threads[temp] = true;
-                                    if(System.currentTimeMillis()>endTime) endTime= System.currentTimeMillis();
-                                synchronized (this) {
-                                    notify();
-                                }
-                            }
-
-                        };
-                    }
-                }
-                for (int k=0; k<poolLength; k++) {
-                    pool[k].start();
-                }
-
-                for (int k=0; k<poolLength; k++) {
-                    try{
-                        if (pool[k].isAlive()) pool[k].join();
-                        System.out.println("Waiting for thread " + k + " to complete");
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                }
-
-                    System.out.print(("P4\n" + N + " " + N + "\n").getBytes());
-                    for(int i=0;i<N;i++) System.out.println(out[i]);
-
-
-                inputText.setText("Done!");
                 if(System.currentTimeMillis()>endTime) endTime= System.currentTimeMillis();
 
                 if(mainThread) totalTime = endTime - startTime;
                 System.out.println("end time is: "+endTime);
                 System.out.println("Total time is: "+totalTime);
+                inputText.setText(String.valueOf(totalTime));
+    }
+
+
+    protected  void doJob(){
+        Crb = new double[N + 7];
+        Cib = new double[N + 7];
+        double invN = 2.0 / N;
+        for (int i = 0; i < N; i++) {
+            Cib[i] = i * invN - 1.0;
+            Crb[i] = i * invN - 1.5;
+        }
+        yCt = new AtomicInteger();
+        out = new byte[N][(N + 7) / 8];
+
+        poolLength = 1;
+        threads = new boolean[poolLength];
+            pool = new Thread[poolLength];
+            for (int i = 0; i < pool.length; i++) {
+                pool[i] = new Thread() {
+                    int temp =0;
+                    public void run() {
+                        int y;
+                        while ((y = yCt.getAndIncrement()) < out.length) {
+                            putLine(y, out[y]);
+                        }
+                        threads[temp] = true;
+                        if(System.currentTimeMillis()>endTime) endTime= System.currentTimeMillis();
+                        synchronized (this) {
+                            notify();
+                        }
+                    }
+
+                };
             }
-        });
+        for (int k=0; k<poolLength; k++) {
+            pool[k].start();
+        }
+
+        for (int k=0; k<poolLength; k++) {
+            try{
+                if (pool[k].isAlive()) pool[k].join();
+                System.out.println("Waiting for thread " + k + " to complete");
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        System.out.print(("P4\n" + N + " " + N + "\n").getBytes());
+        for(int i=0;i<N;i++) System.out.println(out[i]);
 
     }
 
