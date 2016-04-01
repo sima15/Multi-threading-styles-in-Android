848d9c66e07a7c1a37483032da486affa5d172c5
sima15 <s.mehry.89@gmail.com>
2016-03-31 21:47:54 -0600

    improved Spectralnotm Forkjoin

--- experiments/SpectralNorm/SpectralnormForkjoin3/app/src/main/java/com/example/sima/spectralnormforkjoin/MainActivity.java
+++ experiments/SpectralNorm/SpectralnormForkjoin3/app/src/main/java/com/example/sima/spectralnormforkjoin/MainActivity.java
@@ -15,16 +15,12 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     long startTime;
+    int n = 16;
     int numThread = 4;
     ForkJoinPool pool;
-    Approximate[] threads;
-    int appIndex = 5;
 
-    int range;
+    int range = n;
     double m_vBv = 0, m_vv = 0;
-//    private double[] _u;
-//    private double[] _v;
-//    private double[] _tmp;
 
     double[] uArray;
     double[] vArray;
@@ -37,11 +33,10 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Start time: " + startTime);
 
         final NumberFormat formatter = new DecimalFormat("#.000000000");
-        int n = 16;
+
         try {
             for (int i = 0; i < 1; i++) {
                 System.out.println("result is: " + formatter.format(spectralnormGame(n)));
-                Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -49,59 +44,22 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("total time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
 
-    private final double spectralnormGame(int n) throws InterruptedException {
-        // create unit vector
-        double[] u = new double[n];
-        double[] v = new double[n];
-        double[] tmp = new double[n];
 
-        for (int i = 0; i < n; i++)
-            u[i] = 1.0;
-
-        // get available processor, then set up syn object
-        System.out.println("No. of threads: " + numThread);
-
-        int chunk = n / numThread;
-        pool = new ForkJoinPool(numThread);
-        threads = new Approximate[numThread];
-
-        for (int i = 0; i < numThread; i++) {
-            int r1 = i * chunk;
-            int r2 = (i < (numThread - 1)) ? r1 + chunk : n;
-
-            threads[i] = new Approximate(u, v, tmp, r1, r2);
-//            future = pool.submit(threads[i]);
-            pool.submit(threads[i]);
-            range += r2 - r1;
-        }
-
-        uArray = new double[range];
-        vArray = new double[range];
-
-        pool.shutdown();
-        pool.awaitTermination(500, TimeUnit.SECONDS);
-
-        for (int i = 0; i < range; i++) {
-            m_vBv += uArray[i] * vArray[i];
-            m_vv += vArray[i] * vArray[i];
-        }
-        return Math.sqrt(m_vBv / m_vv);
-    }
 
     public class Approximate extends RecursiveAction {
+
         private double[] _u;
         private double[] _v;
         private double[] _tmp;
+
         private static final long serialVersionUID = 6136927121059165206L;
-        int THRESHOLD = 0;
+        int THRESHOLD = 4;
 
 
-        private int range_begin, range_end;
+        private int range_begin;
+        private int range_end;
 
-        //        public Approximate(double[] u, double[] v, double[] tmp, int rbegin, int rend, double num1, double num2) {
         public Approximate(double[] u, double[] v, double[] tmp, int rbegin, int rend) {
-
-//            super();
 
             _u = u;
             _v = v;
@@ -110,32 +68,6 @@ public class MainActivity extends AppCompatActivity {
             range_begin = rbegin;
             range_end = rend;
         }
-
-        public Approximate(double[] v, double[] tmp, int r1, int r2) {
-
-            try {
-                MultiplyAv(v, tmp);
-                range_begin = r1;
-                range_end = r2;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-//            MultiplyAtAv( u,v, tmp);
-//            MultiplyAtAv( _v, _tmp, _u);
-        }
-
-        public Approximate(double[] v, double[] tmp, int r1, int r2, int atv) {
-            try {
-                MultiplyAtv(v, tmp);
-
-                range_begin = r1;
-                range_end = r2;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
 
         /* return element i,j of infinite matrix A */
         private final double eval_A(int i, int j) {
@@ -169,89 +101,64 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(Thread.currentThread().getName() + " finished MultiplyAtv");
         }
         /* multiply vector v by matrix A and then by matrix A transposed */
-//        private final void MultiplyAtAv(final double[] v, double[] tmp, double[] AtAv) {
-//            try {
-//                MultiplyAv(v, tmp);
-//                // all thread must syn at completion
-//                MultiplyAtv(tmp, AtAv);
-//                // all thread must syn at completion
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
+        private final void MultiplyAtAv(final double[] v, double[] tmp, double[] AtAv) {
+            try {
+                MultiplyAv(v, tmp);
+                // all thread must syn at completion
+                MultiplyAtv(tmp, AtAv);
+                // all thread must syn at completion
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
 
             @Override
             public void compute () {
-                if (appIndex >= THRESHOLD) {
-                    appIndex--;
-                    Approximate ap11 = new Approximate(_tmp, _v, range_begin, range_end);
-                    Approximate ap12 = new Approximate(_tmp, _v, range_begin, range_end);
-                    Approximate ap13 = new Approximate(_tmp, _v, range_begin, range_end);
-                    Approximate ap14 = new Approximate(_tmp, _v, range_begin, range_end);
-
-                    ap11.fork();
-                    ap12.fork();
-                    ap13.fork();
-                    ap14.fork();
-
-                    System.out.println("Index is: " + appIndex);
-
-                    Approximate ap21 = new Approximate(_tmp, _u, range_begin, range_end);
-                    Approximate ap22 = new Approximate(_tmp, _u, range_begin, range_end);
-                    Approximate ap23 = new Approximate(_tmp, _u, range_begin, range_end);
-                    Approximate ap24 = new Approximate(_tmp, _u, range_begin, range_end);
-
-                    ap21.fork();
-                    ap22.fork();
-                    ap23.fork();
-                    ap24.fork();
-
-//                Approximate ap2 = new Approximate(_v, _tmp, _u);
-                    Approximate ap31 = new Approximate(_u, _tmp, range_begin, range_end, 5);
-                    Approximate ap32 = new Approximate(_u, _tmp, range_begin, range_end, 5);
-                    Approximate ap33 = new Approximate(_u, _tmp, range_begin, range_end, 5);
-                    Approximate ap34 = new Approximate(_u, _tmp, range_begin, range_end, 5);
-
-                    ap31.fork();
-                    ap32.fork();
-                    ap33.fork();
-                    ap34.fork();
-
-                    Approximate ap41 = new Approximate(_v, _tmp, range_begin, range_end, 5);
-                    Approximate ap42 = new Approximate(_v, _tmp, range_begin, range_end, 5);
-                    Approximate ap43 = new Approximate(_v, _tmp, range_begin, range_end, 5);
-                    Approximate ap44 = new Approximate(_v, _tmp, range_begin, range_end, 5);
-
-                    ap41.fork();
-                    ap42.fork();
-                    ap43.fork();
-                    ap44.fork();
-
-                    ap11.join();
-                    ap12.join();
-                    ap13.join();
-                    ap14.join();
-
-                    ap21.join();
-                    ap22.join();
-                    ap23.join();
-                    ap24.join();
-
-                    ap31.join();
-                    ap32.join();
-                    ap33.join();
-                    ap34.join();
-
-                    ap41.join();
-                    ap42.join();
-                    ap43.join();
-                    ap44.join();
-                } else {
+                if((range_end-range_begin)<THRESHOLD){
+                    computeDirectly();
                     return;
                 }
+
+                int middle = range_begin + (range_end-range_begin)/2;
+                invokeAll(new Approximate(_u, _v, _tmp, range_begin, middle),
+                        new Approximate(_u, _v, _tmp, middle, range_end));
+
             }
+
+        public void computeDirectly(){
+            System.out.println("r1= "+ range_begin + " r2= "+ range_end);
+            MultiplyAtAv(_u, _tmp, _v);
+            MultiplyAtAv(_v, _tmp, _u);
         }
+    }
+
+    private final double spectralnormGame(int n) throws InterruptedException {
+        // create unit vector
+        double[] u = new double[n];
+        double[] v = new double[n];
+        double[] tmp = new double[n];
+
+        for (int i = 0; i < n; i++)
+            u[i] = 1.0;
+
+        pool = new ForkJoinPool(numThread);
+
+        uArray = new double[range];
+        vArray = new double[range];
+
+        Approximate task = new Approximate(u, v, tmp, 0, n);
+        pool.invoke(task);
+        pool.shutdown();
+        pool.awaitTermination(500, TimeUnit.SECONDS);
+
+        for (int i = 0; i < range; i++) {
+            m_vBv += uArray[i] * vArray[i];
+            m_vv += vArray[i] * vArray[i];
+        }
+        return Math.sqrt(m_vBv / m_vv);
+    }
 }
+
 
 
